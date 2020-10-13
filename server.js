73bdd508345decb49f440546f97753dcb1591a7e
vp <vepant@gmail.com>
2020-10-14 00:22:45 +0800

    use apollo-server-express for static files

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const { ApolloServer, gql } = require("apollo-server")
+const express = require("express")
+const { ApolloServer, gql } = require("apollo-server-express")
 
 const resolvers = require("./graphql/resolvers")
 const typeDefs = require("./graphql/typeDefs")
@@ -8,6 +9,23 @@ const server = new ApolloServer({
   resolvers,
 })
 
-server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-  console.log(`Server running on port ${url}`)
-})
+const app = express()
+server.applyMiddleware({ app })
+
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"))
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
+}
+
+//Apollo server without middleware
+// server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+//   console.log(`Server running on port ${url}`)
+// })
+
+app.listen({ port: process.env.PORT || 4000 }, () =>
+  console.log(`Server ready at http://localhost:4000${server.graphqlPath}`)
+)
